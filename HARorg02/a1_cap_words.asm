67323fcc39ed35bb5cc5d6d82f867592e080bd9f
supertoastlional <supertoastlional@gmail.com>
2023-03-23 21:11:11 +0100

    finished the first task of the assembly homework

--- HARorg02/a1_cap_words.asm
+++ HARorg02/a1_cap_words.asm
@@ -48,10 +48,60 @@ main:
 	
 .data
 
-test_string: .asciiz "Nur aNfangsbuchstaben ZAEhlen als gROss geschrieben"
+# test_string: .asciiz "Nur aNfangsbuchstaben ZAEhlen als gROss geschrieben" 
+
+# test_string: .asciiz "hier sind nur Substantive gross: Affe, Banane, Clown, denken"
+# test_string: .asciiz "eins 1, Zwei 2, Drei 3, A, B, C"
+# test_string: .asciiz "Kurz Aber VIELE as: Aa A Aa A AaaaAa"
+# test_string: .asciiz "eIn bUchstAbE GrOss rEIcht nIcht;"
 
 .text
 
-cap_words:
+# $a0: string[]
+# $s0: cap_count
+# $s1: i
 
-	jr $ra
+cap_words:
+	addi $sp, $sp, -8     # mache platz fuer 2 worte auf stack 
+	sw $s0, 0($sp)        # sichere $s0 (cap_count) auf stack
+	sw $s1, 4($sp)        # sichere $s1 (i) auf stack
+
+	addi $s0, $zero, 0    # cap_count = 0
+	addi $s1, $zero, 0    # i = 0
+
+while:
+	lb $t0, 0($a0)            # $t0 = string[i] 
+	beq $t0, $zero, endwhile  # if (string[i] == '\0'), goto endwhile
+
+	beq $s1, $zero, if    # if (i == 0), goto if
+	lb $t1, -1($a0)       # lade string[i-1] 
+	bne $t1, ' ', else    # if (string[i-1] != ' '), goto else 
+
+if:	
+	blt  $t0, 'A', else_if # if (string[i] < 'Z'), goto else_if
+	bgt  $t0, 'Z', else_if # if (string[i] > 'A'), goto else_if
+	addi $s0, $s0, 1      # increment cap_count
+
+	j next                # goto next iteration
+
+else_if:
+	bge $t0, 'A' next     # if (string[i] >= 'Z'), goto next
+	blt $t0, 'Z' next     # if (string[i] < 'A'), goto next
+	addi $s0, $s0, 1      # increment cap_count
+
+	j next                # goto next iteration
+
+else:
+	j next                # goto next iteration
+
+next:
+	addi $a0, $a0, 1      # string pointer inkrementieren 
+	addi $s1, $s1, 1      # i++ 
+	j while               # goto while
+
+endwhile:
+	add $v0, $s0, $zero   # return cap_count
+	lw $s0, 0($sp)        # cap_count ($s0) widerherstellen 
+	lw $s1, 4($sp)        # i ($s1) widerherstellen
+	addi $sp, $sp, 8      # stackpointer wiederherstellen 
+	jr $ra                # Ruecksprung zu main 
--- HARorg02/a1capwords.c
+++ HARorg02/a1capwords.c
@@ -6,6 +6,11 @@ char str_info1[] = "test_string: \"";
 char str_info2[] = "\"\nRueckgabewert cap_words(test_string): ";
 char test_string[] = "Nur aNfangsbuchstaben ZAEhlen als gROss geschrieben";
 
+//char test_string[] = "hier sind nur Substantive gross: Affe, Banane, Clown, denken";
+//char test_string[] = "eins 1, Zwei 2, Drei 3, A, B, C";
+//char test_string[] = "Kurz Aber VIELE as: Aa A Aa A AaaaAa";
+//char test_string[] = "eIn bUchstAbE GrOss rEIcht nIcht;";
+
 
 int main() {
 	printf("%s", str_info1);
@@ -21,19 +26,18 @@ int main() {
 	return 0;
 }
 
- int cap_words(char* string) {
-     int cap_count = 0;
-     int i = 0;
+int cap_words(char* string) {
+	int cap_count = 0;
+	int i = 0;
 
-     while (string[i] != '\0') {
-         if (i == 0 && string[i] >= 'A' && string[i] <= 'Z') {
-             cap_count++;
-         }
-         else if (string[i] >= 'A' && string[i] <= 'Z' && string[i-1] == ' ') {
-             cap_count++;
-         }
-         i++;
-     }
-     return cap_count;
- }
-
+	while (string[i] != '\0') {
+		if (i == 0 && string[i] > 'A' && string[i] < 'Z') {
+			cap_count++;
+		}
+		else if (string[i] >= 'A' && string[i] <= 'Z' && string[i-1] == ' ') {
+			cap_count++;
+		}
+		i++;
+	}
+	return cap_count;
+}
