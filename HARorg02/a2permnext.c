896b7050ac91e3f1ec818143ec273de759fa4309
hoa <supertoastlional@gmail.com>
2023-03-24 13:58:03 +0100

    commit changes

--- HARorg02/a2permnext.c
+++ HARorg02/a2permnext.c
@@ -43,36 +43,73 @@ int main() {
     return 0;
 }
 
-int permnext(unsigned char* perm, int length) {
-	// Save to stack
-    int i = length - 2;
-	int j = 0;
-	int k = 0;
-	int l = 0;
+// int permnext(unsigned char* perm, int length) {
+// 	// Save to stack
+//     int i = length - 2;
+// 	int j = 0;
+// 	int k = 0;
+// 	int l = 0;
 
+//     while (i >= 0 && perm[i] >= perm[i + 1]) {
+//         i--;
+//     }
+
+//     if (i < 0) {
+//         return 0;
+//     }
+
+//     j = length - 1;
+
+//     while (perm[i] >= perm[j]) {
+//         j--;
+//     }
+
+//     swap(perm, i, j);
+
+//     k = i + 1;
+//     l = length - 1;
+
+//     while (k < l) {
+//         swap(perm, k, l);
+//         k++;
+//         l--;
+//     }
+//     return 1;
+// }
+
+int permnext(unsigned char* perm, int length) {
+    int i = length - 2;
+    int j = 0;
+    int k = 0;
+    int l = 0;
+
+    // Find the largest i such that perm[i] < perm[i+1]
     while (i >= 0 && perm[i] >= perm[i + 1]) {
         i--;
     }
 
+    // If no such i exists, then the permutation is the last one
     if (i < 0) {
         return 0;
     }
 
+    // Find the largest j such that perm[i] < perm[j]
     j = length - 1;
-
     while (perm[i] >= perm[j]) {
         j--;
     }
 
+    // Swap perm[i] and perm[j]
     swap(perm, i, j);
 
+    // Reverse the sequence from i+1 to the end of the sequence
     k = i + 1;
     l = length - 1;
-
     while (k < l) {
         swap(perm, k, l);
         k++;
         l--;
     }
+
     return 1;
 }
